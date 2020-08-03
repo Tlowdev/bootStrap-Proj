cb2ef6df26568bfdab507197bf81da0375e0478e
Tylor Roth <tylorroth6@gmail.com>
2020-08-03 13:53:02 -0700

    gitignore

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('.carousel').carousel( {interval: 3000} );
 })
 
 const el = document.querySelectorAll("div.card, div.border");
-el.addEventListner('hover', borderHover);
+el.addEventListner('hover', borderHover());
 function borderHover() {
 document.querySelectorAll("div.card, div.border").style.borderColor = 'red';
 }
